f569255788006e4a238ee3cf622195d9dcea1515
Anuarsin <anuardavid2005@gmail.com>
2024-07-27 21:11:58 -0500

    commitv3

--- main.js
+++ main.js
@@ -6,11 +6,27 @@ document.getElementById('closeContact').addEventListener('click', function() {
     document.getElementById('contactSection').style.display = 'none';
 });
 
-document.getElementById('phoneNumber').addEventListener('click', function() {
-    var phoneNumber = this.textContent;
-    navigator.clipboard.writeText(phoneNumber).then(function() {
-        alert("Número de teléfono copiado al portapapeles: " + phoneNumber);
-    }, function() {
-        alert("Error al copiar el número de teléfono.");
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function() {
+        const copyMessage = document.getElementById('copyMessage');
+        copyMessage.classList.add('show-message');
+
+        setTimeout(function() {
+            copyMessage.classList.remove('show-message');
+        }, 2000);
+    }).catch(function(error) {
+        console.error('Error al copiar:', error);
     });
+}
+
+document.getElementById('phoneLink').addEventListener('click', function(event) {
+    event.preventDefault();
+    const phoneNumber = this.textContent;
+    copyToClipboard(phoneNumber);
+});
+
+document.getElementById('emailLink').addEventListener('click', function(event) {
+    event.preventDefault();
+    const emailAddress = this.textContent;
+    copyToClipboard(emailAddress);
 });
